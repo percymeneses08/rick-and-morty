238e32cb7bf7eed7ec5e5be09abb1a01ad610d4f
percymeneses08 <percymeneses08@gmail.com>
2020-01-02 18:18:23 -0500

    Updates

---
+++ precache-manifest.1ea1780a0853b2678ca10e31f6db6315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a15e90f845be9e6e3492e6a6e3efe0",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "8853656c1ad02bed656a",
+    "url": "/rick-and-morty/static/css/main.a2c6cf84.chunk.css"
+  },
+  {
+    "revision": "da33491b9596e3777104",
+    "url": "/rick-and-morty/static/js/2.9457638d.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/rick-and-morty/static/js/2.9457638d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8853656c1ad02bed656a",
+    "url": "/rick-and-morty/static/js/main.ebad1d7b.chunk.js"
+  },
+  {
+    "revision": "623cd3597bb5aaceb743",
+    "url": "/rick-and-morty/static/js/runtime-main.ac4b2c1f.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/rick-and-morty/static/media/logo.42d6efdb.png"
+  }
+]);
